255209f1c7579f4183b260eb496808c72ab9da04
Muktar16 <bsse1116@iit.du.ac.bd>
2022-12-18 07:47:51 +0600

    Two-way Binding in React

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -17,20 +17,34 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event)=>{
+        event.preventDefault();
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+        const expenseData={
+            title: enteredTitle,
+            amount:enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);  
+    }
+
     return(
-        <form action="">
+        <form action="" onSubmit={submitHandler}>
             <div className="new-expense_controls">
                 <div className="new-expense_control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense_control">
                     <label>Amount</label>
-                    <input type="number" min='0.01' step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" value={enteredAmount} min='0.01' step="0.01" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense_control">
                     <label>Date</label>
-                    <input type="date" onChange={dateChangeHandler}/>
+                    <input type="date" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense_actions">
